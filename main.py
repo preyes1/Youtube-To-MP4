ac6ec6eb25fc32f6bbaec3f24f238ecf4aa42471
preyes1 <79494402+preyes1@users.noreply.github.com>
2024-02-29 12:03:47 -0500

    Added cute anime girl and mp3 download
    Added anime girl at the bottom of the screen for the cuteness and added a mp3 download option.
    Also prohibited the user from resizing the app.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter
 import customtkinter #makes UI nicer
 from pytube import YouTube
+from PIL import Image
 
 
 def convertMP4():
@@ -9,6 +10,17 @@ def convertMP4():
         ytObject = YouTube(ytLink)
         video = ytObject.streams.get_highest_resolution()
         video.download()
+        titleLabel.configure(text=ytObject.title)
+        finishLabel.configure(text="Downloaded", text_color="green")
+    except:
+        finishLabel.configure(text="Download Failed", text_color="red")
+
+def convertMP3():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        mp3 = ytObject.streams.get_audio_only()
+        mp3.download()
         titleLabel.configure(text=ytObject.title)
         finishLabel.configure(text="Downloaded", text_color="green")
     except:
@@ -22,7 +34,14 @@ customtkinter.set_default_color_theme("blue")
 #app frame
 app = customtkinter.CTk()
 app.geometry("500x500")
+app.resizable(False, False)
 app.title("Youtube Downloader")
+image = customtkinter.CTkImage(light_image=Image.open('backgroundanime.png'),
+                                dark_image=Image.open('backgroundanime.png'),
+                                size=(220,310))
+mylabel = customtkinter.CTkLabel(app, text="", image=image)
+mylabel.place(x=280,y=200)
+
 
 #adding UI Elements
 title = customtkinter.CTkLabel(app, text="Insert YouTube Link")
@@ -37,8 +56,11 @@ link.pack()
 finishLabel = customtkinter.CTkLabel(app, text="")
 finishLabel.pack()
 
-#Download Button
-download = customtkinter.CTkButton(app, text="Download", command=convertMP4)
+#Download mp4 Button
+download = customtkinter.CTkButton(app, text="Download MP4", command=convertMP4)
+download.pack(pady=10)
+#Download mp3 Button
+download = customtkinter.CTkButton(app, text="Download MP3", command=convertMP3)
 download.pack(pady=10)
 
 titleLabel = customtkinter.CTkLabel(app, text="")
